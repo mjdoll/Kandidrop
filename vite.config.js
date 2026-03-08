a5d730b8ae04fe0338115e45e9b96c8e1acfbfd4
mjdoll <kandidrop8@gmail.com>
2026-03-08 15:21:31 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
-{
-  "rewrites": [
-    { "source": "/(.*)", "destination": "/index.html" }
-  ]
-}
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+});
